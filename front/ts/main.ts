66e49a4cda2cafd84bd6cdc7cec4a7e2c6a03c65
Bruno Mateus <brunomateus@gmail.com>
2025-02-17 20:27:13 -0300

    Refatoração. Quebrando em funções menores

--- front/ts/main.ts
+++ front/ts/main.ts
@@ -11,7 +11,7 @@ let taskToDelete: string;
 
 deleteButton?.addEventListener('click', async () => {
   await deleteTask(taskToDelete)
-  listarTarefas()
+  mostrarTarefas()
   deleteModal.close()
 })
 
@@ -24,7 +24,7 @@ formDescricao?.addEventListener('input', () => {
 })
 
 
-form?.addEventListener('submit', (e) => {
+form?.addEventListener('submit', async (e) => {
   e.preventDefault();
   const task = {
     description: formDescricao.value,
@@ -35,68 +35,75 @@ form?.addEventListener('submit', (e) => {
       description: selectCategory.options[selectCategory.selectedIndex].text
     }
   }
-  createTask(task)
-  listarTarefas()
+  await createTask(task)
+  mostrarTarefas()
 })
 
+async function mostrarTarefas() {
+  const res =  await getAll();
+  const tableBody = document.getElementById("tarefas")
+  if(tableBody) {
+    tableBody.innerHTML = ''
+    for(let task of res.data) {
+      const novaLinha = document.createElement('tr')
 
-getAllCategory().then(res => {
+      const description = document.createElement('td')
+      description.setAttribute('colspan', '2');
+      description.innerText = task.description
 
-   for(let categoria of res.data) {
-    const option = document.createElement('option')
-    option.value = categoria.documentId
-    option.innerText = categoria.description
-    selectCategory?.appendChild(option)
-   }
-})
+      const category = document.createElement('td')
+      const spanCategory = createCategorLabel(task.category.description)
+      category.appendChild(spanCategory)
 
+      const deadline = document.createElement('td')
+      deadline.innerText = task.deadline || ''
 
-function listarTarefas() {
-  getAll().then(res => {
-  
-    const tableBody = document.getElementById("tarefas")
-    if(tableBody) {
-      tableBody.innerHTML = ''
-      for(let task of res.data) {
-        const novaLinha = document.createElement('tr')
-  
-        const description = document.createElement('td')
-        description.setAttribute('colspan', '2');
-        description.innerText = task.description
-  
-        const category = document.createElement('td')
-        const spanCategory = document.createElement('span')
-        spanCategory.innerText = task.category.description
-        spanCategory.classList.add('category')
-        spanCategory.classList.add(task.category.description.toLocaleLowerCase())
-        category.appendChild(spanCategory)
-  
-        const deadline = document.createElement('td')
-        deadline.innerText = task.deadline || ''
-  
-        const acoes = document.createElement('td')
-        const deleteButton = document.createElement('button')
-        deleteButton.className = 'delete-button'
-        deleteButton.addEventListener('click', (e) => {
-          if(task.documentId) {
-            showDeleteModal(task)
-          }
-        })
-        
-        const icon = document.createElement('i')
-        icon.className = 'bi bi-trash3'
-        deleteButton.appendChild(icon)
-        acoes.appendChild(deleteButton)
-  
-        novaLinha.appendChild(description)
-        novaLinha.appendChild(category)
-        novaLinha.appendChild(deadline)
-        novaLinha.appendChild(acoes)
-        tableBody.appendChild(novaLinha)
-      }
+      const acoes = document.createElement('td')
+      const deleteButton = createDeleteButton(task)
+      acoes.appendChild(deleteButton)
+
+      novaLinha.appendChild(description)
+      novaLinha.appendChild(category)
+      novaLinha.appendChild(deadline)
+      novaLinha.appendChild(acoes)
+      tableBody.appendChild(novaLinha)
+    }
+  }
+
+}
+
+function createOption({ documentId, description}: Category): HTMLOptionElement {
+  const option = document.createElement('option')
+  option.value = documentId
+  option.innerText = description
+  return option
+}
+
+function createCategorLabel(description: string): HTMLSpanElement {
+  const spanCategory = document.createElement('span')
+  spanCategory.innerText = description
+  spanCategory.classList.add('category')
+  spanCategory.classList.add(description.toLocaleLowerCase())
+  return spanCategory
+}
+
+function createDeleteButton(task: Task): HTMLButtonElement {
+  const deleteButton = document.createElement('button')
+  deleteButton.className = 'delete-button'
+  deleteButton.addEventListener('click', (e) => {
+    if(task.documentId) {
+      showDeleteModal(task)
     }
   })
+  
+  const icon = document.createElement('i')
+  icon.className = 'bi bi-trash3'
+  deleteButton.appendChild(icon)
+ 
+
+  return deleteButton
 }
+
 
 function showDeleteModal(task: Task) {
   deleteModal?.showModal()
@@ -105,4 +112,15 @@ function showDeleteModal(task: Task) {
   taskToDelete = task.documentId as string
 }
 
-listarTarefas()
+
+window.addEventListener('load', async () => {
+
+  const res = await getAllCategory();
+  for(let categoria of res.data) {
+    const option = createOption(categoria)
+    selectCategory?.appendChild(option)
+  }
+
+  mostrarTarefas()
+ 
+});
